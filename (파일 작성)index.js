16e08c9d21c1154e18ee8fec2b9068b60888d7c9
LouiIII3 <119919129+LouiIII3@users.noreply.github.com>
2023-06-16 13:05:57 +0900

    Update (파일 작성)index.js

--- (파일 작성)index.js
+++ (파일 작성)index.js
@@ -1,6 +1,32 @@
-const express = require('express')
-const app = express()
-app.get('/', function (req, res) {
-res.send('Hello World')
-})
-app.listen(3000)
+//필요한 모듈과 변수를 초기화
+const express = require('express');
+const app = express();
+const port = 8010;
+const bodyParser = require('body-parser');
+
+//서버요청 핸들러 설정
+app.get('/', function(req, res) {
+  // GET 요청 처리 작업
+  res.send("get 요청에 따라 동작합니다.");
+});
+
+app.post('/hosil', function (req, res) {
+  // POST 요청 처리 작업
+});
+
+//POST 요청의 처리 작업을 구현
+app.post('/', function (req, res) {
+  if (req.body["h_sound1"] != null) {
+    // 요청 데이터 확인 및 처리 작업
+  } else {
+    // 오류 처리 작업
+  }
+
+  // 응답 전송
+  res.send(data_h);
+});
+
+//서버 시작
+app.listen(port, () => {
+  console.log(`웹 서버가 ${port}포트로 실행 중입니다.`);
+});
